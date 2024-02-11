540bb720f223da354f840c9be26c1477482ed190
Dajana <black.inez2401@gmail.com>
2024-02-11 15:11:46 +0100

    1 test using actions

--- cypress/e2e/APITestGET.cy.js
+++ cypress/e2e/APITestGET.cy.js
@@ -14,17 +14,17 @@ describe ('API test GET response',()=>{
             })
         });
         });
-        describe ('GET response on wrong url',()=>{
-            const wrongUrl={
-                url: 'https://httpbin.org/not-exist',
-                failOnStatusCode:false
-            }
-            it('response should not be 200',()=>{
-                cy.request(wrongUrl).then(response=>{
-                    const currentStatus=response.status;
-                    const expectedStatus=200;
-                    assert.equal(expectedStatus,currentStatus);
-                    cy.log(currentStatus);
-                })
-            });
-            });
+        // describe ('GET response on wrong url',()=>{
+        //     const wrongUrl={
+        //         url: 'https://httpbin.org/not-exist',
+        //         failOnStatusCode:false
+        //     }
+        //     it('response should not be 200',()=>{
+        //         cy.request(wrongUrl).then(response=>{
+        //             const currentStatus=response.status;
+        //             const expectedStatus=200;
+        //             assert.equal(expectedStatus,currentStatus);
+        //             cy.log(currentStatus);
+        //         })
+        //     });
+        //     });
